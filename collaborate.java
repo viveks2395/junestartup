4292c740acff409257204dea79e1fc0e1c38036d
svivek2395 <svivek2395@gmail.com>
2023-07-11 00:00:49 +0530

    add more details work on it.....

---
+++ collaborate.java
@@ -0,0 +1,23 @@
+public static void main(String []args){
+byte rollno = 23;
+String sname = "Vivek";
+char gender = 'M';
+char grade = 'A';
+System.out.println("*********************************");
+System.out.println("Name of the Student: "+ sname);
+System.out.println("Gender of the Student: "+ gender);
+System.out.println("RollNo of the Student: "+ rollno);
+System.out.println("Grade of the Student: "+ grade);
+System.out.println("*********************************");
+
+
+
+
+
+
+
+
+
+
+
+}	
