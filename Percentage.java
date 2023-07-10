1eb40e597af3243533b282fcf63ffb96bd4d7d18
svivek2395 <svivek2395@gmail.com>
2023-07-10 23:59:38 +0530

    ssss

--- Percentage.java
+++ Percentage.java
@@ -2,9 +2,9 @@ package automat;
 
 public  class Percentage {
 	public static void main(String[] args) {
-	double sub1= 35;
-	double sub2= 34;
-	double sub3= 35;
+	double sub1= 80;
+	double sub2= 90;
+	double sub3= 80;
 	double per= (sub1+sub2+sub3)*100/(3*100);
 
 	
